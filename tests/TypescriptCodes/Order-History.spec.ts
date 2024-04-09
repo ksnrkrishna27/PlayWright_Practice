cbc74bbdb9aa3cca491180924878ec65a2a55233
Rama Krishna <ksnrkrishna27@gmail.com>
2024-04-09 14:16:53 +0530

    Order History flow2

--- tests/TypescriptCodes/Order-History.spec.ts
+++ tests/TypescriptCodes/Order-History.spec.ts
@@ -14,7 +14,30 @@ test("Order History flow1", async()=>{
 
     await new_page.screenshot({path : './Screenshots/Order_History_flow1.png'});
 
+    await new_page.waitForTimeout(2000);
 
     await logout(new_page);
+
+})
+
+test("Order History flow2", async() =>{
+
+    const new_page : Page = await login("abcz1@gmail.com","abc123");
+
+    const My_Account_locator : Locator = await new_page.locator('//*[@id="top-links"]/ul/li[2]/a/span[1]');
+    await My_Account_locator.click();
+
+    const order_history_locator : Locator = await new_page.locator('//*[@id="top-links"]/ul/li[2]/ul/li[2]/a');
+    await order_history_locator.click();
+
+    let order_history_title : string = await new_page.title();
+    expect(order_history_title).toEqual("Order History");
+
+    await new_page.screenshot({path : './Screenshots/Order_History_flow2.png'});
+
+    await new_page.waitForTimeout(2000);
+
+    await logout(new_page);
+
 
 })
