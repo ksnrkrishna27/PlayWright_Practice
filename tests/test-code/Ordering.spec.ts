bff6764f43ab46882e094dead280cb2fceeae1cb
ksnrkrishna27 <ksnrkrishna27@gmail.com>
2025-05-16 01:07:54 +0530

    update

--- tests/test-code/Ordering.spec.ts
+++ tests/test-code/Ordering.spec.ts
@@ -1,32 +1,32 @@
-import {test,expect,Page,Locator} from "@playwright/test"
+import { test, expect, Page, Locator } from "@playwright/test";
 
-import {login,logout} from "../ReusableCode/util"
+import { login, logout } from "../ReusableCode/util";
 
-let email_id : string = "abcz1@gmail.com";
-let password : string = "abc123";
+let email_id: string = "abcz1@gmail.com";
+let password: string = "abc123";
 
-test("Ordering item", async ()=>{
+test("Ordering item", async () => {
+  const page_after_login: Page = await login(email_id, password);
 
-    const page_after_login : Page = await login(email_id,password);
+  // const width = await page_after_login.evaluate(() => window.outerWidth);
+  //     const height = await page_after_login.evaluate(() => window.outerHeight);
+  //     await page_after_login.setViewportSize({width,height});
 
-    // const width = await page_after_login.evaluate(() => window.outerWidth);
-    //     const height = await page_after_login.evaluate(() => window.outerHeight);
-    //     await page_after_login.setViewportSize({width,height});
+  const download_locator: Locator = await page_after_login.locator(
+    '//*[@id="column-right"]/div/a[7]'
+  );
 
-    const download_locator : Locator = await page_after_login.locator('//*[@id="column-right"]/div/a[7]');
+  await download_locator.click();
 
-    await download_locator.click();
+  const download_page_title: string = await page_after_login.title();
 
-    const download_page_title : string = await page_after_login.title();
+  console.log(download_page_title);
 
-    console.log(download_page_title);
-    
-    await page_after_login.screenshot({path : './Screenshots/download_page.png'});
+  await page_after_login.screenshot({
+    path: "./Screenshots/download_page.png",
+  });
 
-    expect(download_page_title).toEqual("Account Downloads");
+  expect(download_page_title).toEqual("Account Downloads");
 
-    await logout(page_after_login);
-
-    
-
-})
+  await logout(page_after_login);
+});
--- tests/test-code/Register.spec.ts
+++ tests/test-code/Register.spec.ts
@@ -1,31 +1,37 @@
-import {test,expect,Locator,Page, Browser, chromium} from "@playwright/test"
-import { login } from "../ReusableCode/util"
+import {
+  test,
+  expect,
+  Locator,
+  Page,
+  Browser,
+  chromium,
+} from "@playwright/test";
+import { login } from "../ReusableCode/util";
 
-test("Registering a User",{tag:'@Register'}, async() => {
-    
-    const browser : Browser = await chromium.launch();
-    const page : Page = await browser.newPage();
-    await page.goto("https://naveenautomationlabs.com/opencart/index.php?route=account/register");
+test("Registering a User", { tag: "@Register" }, async () => {
+  const browser: Browser = await chromium.launch();
+  const page: Page = await browser.newPage();
+  await page.goto(
+    "https://naveenautomationlabs.com/opencart/index.php?route=account/register"
+  );
 
-    const firstname_locator : Locator =  page.locator('[name="firstname"]')
-    await firstname_locator.fill("abc");
+  const firstname_locator: Locator = page.locator('[name="firstname"]');
+  await firstname_locator.fill("abc");
 
-    const lastname_locator : Locator =  page.locator('[name="lastname"]')
-    await lastname_locator.fill("def");
+  const lastname_locator: Locator = page.locator('[name="lastname"]');
+  await lastname_locator.fill("def");
 
-    const email_locator : Locator =  page.locator('[name="email"]');
-    await email_locator.fill("abcz1@gmail.com");
+  const email_locator: Locator = page.locator('[name="email"]');
+  await email_locator.fill("abcz1@gmail.com");
 
-    const phonenum_locator : Locator =  page.locator('[name="telephone"]');
-    await phonenum_locator.fill("1234567891");
+  const phonenum_locator: Locator = page.locator('[name="telephone"]');
+  await phonenum_locator.fill("1234567891");
 
-    const pw_locator : Locator =  page.locator('[name="password"]');
-    await pw_locator.fill("abc123");
+  const pw_locator: Locator = page.locator('[name="password"]');
+  await pw_locator.fill("abc123");
 
-    const confirmpw_locator : Locator = page.locator('[name="confirm"]');
-    await confirmpw_locator.fill("abc123");
+  const confirmpw_locator: Locator = page.locator('[name="confirm"]');
+  await confirmpw_locator.fill("abc123");
 
-
-    page.close();
-
-})
+  page.close();
+});
